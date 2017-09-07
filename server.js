9faa44b0e11f4ac41e4aa42151dbcb62980e2007
morgan <morgan.ottosson-cic@se.ibm.com>
2017-09-07 11:30:17 +0200

    inital commit with files

---
+++ server.js
@@ -0,0 +1,20 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var DEFAULT_PORT = 1337;
+var port = '';
+
+if(!process.env.port) {
+    port = DEFAULT_PORT;
+} else {
+    port = process.env.port;
+}
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(port, () => {
+    console.log('Server listening on port: ', port);
+});
